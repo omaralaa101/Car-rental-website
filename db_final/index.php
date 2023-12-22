9c8193f48a9f385e732cc766366a51fdb9e74c78
Yehia Tarek Selim <129012928+Cyberwire666@users.noreply.github.com>
2023-12-22 20:36:11 +0200

    Add files via upload

---
+++ db_final/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>car rent</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header>
+        <a href="#" class="logo"><img src="img/jeep.png" alt=""></a>
+        <div id="menu"><box-icon name='menu'></box-icon></div>
+        <ul class="navbar">
+            <li> <a href="#home">home</a></li>
+            <li> <a href="#ride">ride</a></li>
+            <li> <a href="#services">services</a></li>
+            <li> <a href="#about">about</a></li>
+            <li> <a href="#reviews">reviews</a></li>
+        </ul>
+        <div class="header-btn">
+            <a href="register_form.php" class="sign-up">sign up</a>
+            <a href="login_form.php" class="sign-in">sign in</a>
+        </div>
+        <a href="#"></a>
+    </header>
+    <!--home-->
+    <section class="home" id="home">
+     <div class="text">  
+     <h1><span>Looking</span> to <br>rent a car</h1> 
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, repudiandae doloribus?</p>
+     <div class="app-stores">
+        <img src="img/ios.png" alt="">
+        <img src="img/512x512.png" alt="">
+        
+    </div>
+    </div>
+   <!--<div class="form-container"> 
+        <form action="">
+        <div class="input-box">
+           <span>Location</span>
+           <input type="search" name="" id="" placeholder="search places">
+        </div>
+         <div class="input-box">
+           <span>Pick-up Date</span>
+           <input type="date" name="" id=""> 
+         </div>
+         <div class="input-box">
+            <span>Return Date</span>
+            <input type="date" name="" id=""> 
+          </div> 
+          <input type="submit" name="" id="" class="btn">   
+        </form>
+    </div>--> 
+
+    </section>
+   <!--ride-->
+   <section class="ride" id="ride">
+    <div class="heading">
+      <span>How it work</span> 
+      <h1>Rent with 3 Easy steps</h1> 
+    </div>
+    <div class="ride-container">
+        <div class="box">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAkNJREFUSEvF1knITlEcx/HPi5AFsqNkypQFWZBhg4WsRLFCbEhsjAs7KRaGjSgpmVYiS5IQKSJDCUWIbBBLSYb7r/vUfW93OM/7Lt7/5um55/c/3/M/w++cHgMUPQPE1RfwBMzFRPzDBzzBx26KSAWHbgt2YmoN4DWO4Ew+oMZxpIDH4SrmJVZ0D6vxrUnfBp6GOxibCO3IPmER4rcymsAj8QyTuoR25E+xAL+q8pvA57G+BnoXt/O2ZVhcozuGXd2A5+NBRcIrrMWLUttsXMGU0ve/mIE35b7qKo6R7iiJv+TH6HNNdbEkMb2jSu178t3e63MdOCqaVepgY7be51rWezcOlzQ3sDy14h8YXRKPQXxviumI81yM95icCv6NISVx/P/TAh6OnyVN/B+RCn5XcYzCKttsMdY5covxtsrt6tb4JuKYFGMbTrZUHJZ6tKS5jhWpFR/PLG97SfwdcxrcKDz8McJ4ihGbbW8qOHz5YUV1sXHWVJzj0F/G+IqcmRUbrvFafIlIqopb2VLEZRCxMp+JKl34/JKqhibL3JBwbluW3NKCtfbStt1O97Gwrfea9gvZDo/BV0YbOAzhOYZ1Cf+KyK01nDZw8Dblr4pUdphP3MWPmhJSwJF/CpsTyXGVXmzTpoLDLi9l076qpcND2NcGjfZUcGgHZy/Ls1hX0/HpLmalK3BnoCewtQCPy/5AVsT+lNdlJ6+biouFhp0exKDcya6lTG9R01dw9BEv0KEV9pk0hv6AkwB9NZB+dd6U/B9sMVkf9PcpxwAAAABJRU5ErkJggg=="/>
+            <h2>choose a Location </h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ipsa necessitatibus perferendis veritatis vitae veniam?</p> 
+        </div>
+        
+        <div class="box">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARhJREFUSEvl1jFKBEEQheFvc8HNTcw0FIwUBL2AYGboDbyJZzDdxNRQE1FYAzPByMADGG246soMrMN0U9gDvehEQ/Oq/q7X1TM1UukZVeKKgDdx1mzw8uv9NbHZqO47PAI+xE0DO8JtAhzVrR74ABfYDTqSa5MPPOIc98vCrtXreMPawE33jg3M2rxd8D7uBoa26fbwkAIvN8jQ/B+N2a14JcC5qxN1I3nFchX/X/AYO423T1hcjehakdV9wSVrvZ/MkoTR2DA4amufrsjq6NXp0xWBq1UcPbuoLnzG0YRRXRhczepqzfW3wdt4LikxE7uFl9QEsli/wsnA8AlOc8NeO2sfN3+kyNyd2+McU1x3RaWJf21MNfAnsuaMH2909zcAAAAASUVORK5CYII="/>
+            <h2>Pick-up Date </h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ipsa necessitatibus perferendis veritatis vitae veniam?</p> 
+        </div>
+
+        <div class="box">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAYtJREFUSEvtls8rBVEUxz/PWrGXsmOpbPyOslY2srQSESvlr1AiP3bKioWtJRtRLKT8LlGkZCELG4U5OlPX7d2ZO+/d1zxlVrczZ87nnu+ce+4pkNNTyImLD7gJGNUNrkfrO8dmff1+PvcB9wG7CusH9hxgX7/qA/cA80CbpyJJZfIFHAMzwIHpaEtdBzwAtYGL7hVoAN7juDa4E9gPDI3DdQCHLrBZIKH5vwrTzrgqwElHx1cN5xFLyvjPgGv0NLwZclQ8Y1FuA2gEBoAPhVccvAKMK2zNWAcBDwOXwKlVWUvApNqkUUhtnITKuFsvCvl/7cCNBl4GJnT9El0gkuFZyH+8AExrwCegCxgD5tT2rJmeW2qULbUUz6rCJLZIWm9Ae4GrIoe7bHAccxGYMgCPgEBvs97RpTSQGC5Qkfw+oY0FyzhmzAJbKVDxDQ7+79UuBbylbgEufHXM6NcMXLsmELFvA0MZg6a5b0btdsR0KjZXi20QaA0wZX4CR8COvTOfgT4tm5Le5wb+BmaifR+HkqhaAAAAAElFTkSuQmCC"/>
+            <h2>Book a car </h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ipsa necessitatibus perferendis veritatis vitae veniam?</p> 
+        </div>
+    </div>
+   </section>
+   <!--service-->
+   <section class="services" id="services">
+    <div class="heading">
+        <span>Best services</span> 
+        <h1>Explore our top deals<br>from our top rated Dealers</h1> 
+      </div>
+      <div class="services-container">
+        <div class="box">
+        <div class="box-img">
+       <img src="img/car1.jpg" alt=""> 
+        </div>
+        <p>2017</p>
+        <h3>2018 Honda</h3>
+        <h2>200,000 egp</h2>
+        <a href="rent.php" class="btn">Rent now</a>
+      </div>
+      
+      
+      <div class="box">
+        <div class="box-img">
+           <img src="img/car2.jpg" alt="">
+        </div>
+        <p>2017</p>
+        <h3>2018 Honda</h3>
+        <h2>200,000 egp</h2>
+        <a href="rent.php" class="btn">Rent now</a>
+      </div>
+
+      <div class="box">
+        <div class="box-img">
+          <img src="img/car3.jpg" alt="">
+        </div>
+        <p>2017</p>
+        <h3>2018 Honda</h3>
+        <h2>200,000 egp</h2>
+        <a href="rent.php" class="btn">Rent now</a>
+      </div>
+
+      <div class="box">
+        <div class="box-img">
+           <img src="img/car4.jpg" alt=""> 
+        </div>
+        <p>2017</p>
+        <h3>2018 Honda</h3>
+        <h2>200,000 egp</h2>
+        <a href="rent.php" class="btn">Rent now</a>
+      </div>
+
+      <div class="box">
+        <div class="box-img">
+           <img src="img/car5.jpg" alt="">
+        </div>
+        <p>2017</p>
+        <h3>2018 Honda</h3>
+        <h2>200,000 egp</h2>
+        <a href="rent.php" class="btn">Rent now</a>
+      </div>
+
+      <div class="box">
+        <div class="box-img">
+          <img src="img/car6.jpg" alt=""> 
+        </div>
+        <p>2017</p>
+        <h3>2018 Honda</h3>
+        <h2>200,000 egp</h2>
+        <a href="rent.php" class="btn">Rent now</a>
+      </div>
+    </div>
+       </section>
+   <!--about-->
+   <section class="about" id="about">
+    <div class="heading">
+      <span>About us</span> 
+      <h1>Best customer Experience</h1> 
+    </div>
+    <div class="about-container">
+      <div class="about-img">
+        <img src="img/about.png" alt="">
+      </div>
+      <div class="about-text">
+        <span>About us</span>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae excepturi eligendi deserunt quaerat eos, amet voluptate tenetur quas rem, aut laudantium quam? Mollitia et iure possimus nesciunt fuga omnis deleniti beatae? Incidunt, eaque tenetur?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat vel consequuntur, repudiandae deleniti iste autem repellendus alias obcaecati?</p>
+        <a href="#" class="btn">Learn more</a>
+      </div>
+    </div>
+   </section>
+   <div class="copyright">
+    <p>$#169;cybersecurity ANU</p>
+    <div class="social"></div>
+   </div>
+   <!--scroll reveal-->
+   <script src="https://unpkg.com/scrollreveal"></script>
+
+    <script src="main.js"></script>
+    
+</body>
+</html>
